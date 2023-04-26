0e6759216a9391c3ea040d2c17691d78e0382a22
Lasha gorakudashvili <lashatamo11@gmail.com>
2023-04-27 00:52:20 +0400

    only the clean up left

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,7 @@ setInterval(displayTime, 1000);
 
 
 //2
-function mainSlider() {
+/*function mainSlider() {
   const slides = document.querySelectorAll(".slide");
   const next = document.querySelector(".next");
   const prev = document.querySelector(".prev");
@@ -110,27 +110,149 @@ function mainSlider() {
     });
   });
     
-  // Add navbar functionality
-  const navbar = document.querySelector(".navbar");
-  const navCircles = document.querySelectorAll(".nav-circle");
   
-  navCircles.forEach((circle, i) => {
+}
+
+mainSlider();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mainSlider() {
+  const slides = document.querySelectorAll(".slide");
+  const next = document.querySelector(".next");
+  const prev = document.querySelector(".prev");
+  const indicators = document.querySelectorAll(".indicator");
+  const circles = document.querySelectorAll('.circle');
+  const images = document.querySelectorAll('.hidden');
+  
+  let slideIntervalId = null;
+  let activeIndex = 0;
+
+  function renderSlides() {
+    slides.forEach((el, i) => {
+      if (i === activeIndex) {
+        el.classList.add("active");
+      } else {
+        el.classList.remove("active");
+      }
+    });
+    renderIndicators();
+  }
+
+  function renderIndicators() {
+    indicators.forEach((el, i) => {
+      if (i === activeIndex) {
+        el.classList.add("active");
+      } else {
+        el.classList.remove("active");
+      }
+    });
+  }
+  
+  function showImage(index) {
+    images.forEach((image, i) => {
+      if (i === index) {
+        image.classList.add("active");
+      } else {
+        image.classList.remove("active");
+      }
+    });
+  }
+  
+  function hideImages() {
+    images.forEach((image) => {
+      image.classList.remove("active");
+    });
+  }
+
+  function nextFn() {
+    if (activeIndex === slides.length - 1) {
+      activeIndex = 0;
+    } else {
+      activeIndex++;
+    }
+    renderSlides();
+    hideImages();
+    showImage(activeIndex);
+  }
+
+  function prevFn() {
+    if (activeIndex === 0) {
+      activeIndex = slides.length - 1;
+    } else {
+      activeIndex--;
+    }
+    renderSlides();
+    hideImages();
+    showImage(activeIndex);
+  }
+
+  next.addEventListener("click", nextFn);
+  prev.addEventListener("click", prevFn);
+
+  function startSlideInterval() {
+    slideIntervalId = setInterval(nextFn, 5000);
+  }
+
+  function stopSlideInterval() {
+    clearInterval(slideIntervalId);
+  }
+
+  function handleSlideInterval() {
+    stopSlideInterval();
+    startSlideInterval();
+  }
+
+  startSlideInterval();
+  renderSlides();
+  showImage(activeIndex);
+
+  slides.forEach((slide) => {
+    slide.addEventListener("mouseenter", stopSlideInterval);
+    slide.addEventListener("mouseleave", startSlideInterval);
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (e.code === "ArrowRight") {
+      nextFn();
+      handleSlideInterval();
+    }
+    if (e.code === "ArrowLeft") {
+      prevFn();
+      handleSlideInterval();
+    }
+  });
+
+  indicators.forEach((indicator, i) => {
+    indicator.addEventListener("click", () => {
+      activeIndex = i;
+      renderSlides();
+      hideImages();
+      showImage(activeIndex);
+      handleSlideInterval();
+    });
+  });
+  
+  circles.forEach((circle, i) => {
     circle.addEventListener("click", () => {
       activeIndex = i;
       renderSlides();
+      hideImages();
+      showImage(activeIndex);
       handleSlideInterval();
     });
   });
 }
 
 mainSlider();
-
-
-
-
-
-
-
-
-
-

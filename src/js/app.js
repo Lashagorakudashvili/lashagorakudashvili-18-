d64b8844c62bd197ccb91aae232e05ccc8d6eab8
Lasha gorakudashvili <lashatamo11@gmail.com>
2023-04-27 22:18:52 +0400

    done

--- src/js/app.js
+++ src/js/app.js
@@ -20,114 +20,6 @@ setInterval(displayTime, 1000);
 
 
 //2
-/*function mainSlider() {
-  const slides = document.querySelectorAll(".slide");
-  const next = document.querySelector(".next");
-  const prev = document.querySelector(".prev");
-  const indicators = document.querySelectorAll(".indicator");
-  let slideIntervalId = null;
-  let activeIndex = 0;
-
-  function renderSlides() {
-    slides.forEach((el, i) => {
-      if (i === activeIndex) {
-        el.classList.add("active");
-      } else {
-        el.classList.remove("active");
-      }
-    });
-    renderIndicators();
-  }
-
-  function renderIndicators() {
-    indicators.forEach((el, i) => {
-      if (i === activeIndex) {
-        el.classList.add("active");
-      } else {
-        el.classList.remove("active");
-      }
-    });
-  }
-
-  function nextFn() {
-    if (activeIndex === slides.length - 1) {
-      activeIndex = 0;
-    } else {
-      activeIndex++;
-    }
-    renderSlides();
-  }
-
-  function prevFn() {
-    if (activeIndex === 0) {
-      activeIndex = slides.length - 1;
-    } else {
-      activeIndex--;
-    }
-    renderSlides();
-  }
-
-  next.addEventListener("click", nextFn);
-  prev.addEventListener("click", prevFn);
-
-  function startSlideInterval() {
-    slideIntervalId = setInterval(nextFn, 5000);
-  }
-
-  function stopSlideInterval() {
-    clearInterval(slideIntervalId);
-  }
-
-  function handleSlideInterval() {
-    stopSlideInterval();
-    startSlideInterval();
-  }
-
-  startSlideInterval();
-  renderSlides();
-
-  slides.forEach((slide) => {
-    slide.addEventListener("mouseenter", stopSlideInterval);
-    slide.addEventListener("mouseleave", startSlideInterval);
-  });
-
-  document.addEventListener("keyup", (e) => {
-    if (e.code === "ArrowRight") {
-      nextFn();
-      handleSlideInterval();
-    }
-    if (e.code === "ArrowLeft") {
-      prevFn();
-      handleSlideInterval();
-    }
-  });
-
-  indicators.forEach((indicator, i) => {
-    indicator.addEventListener("click", () => {
-      activeIndex = i;
-      renderSlides();
-      handleSlideInterval();
-    });
-  });
-    
-  
-}
-
-mainSlider();*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function mainSlider() {
   const slides = document.querySelectorAll(".slide");
   const next = document.querySelector(".next");
@@ -148,6 +40,7 @@ function mainSlider() {
       }
     });
     renderIndicators();
+    renderCircles();
   }
 
   function renderIndicators() {
@@ -156,6 +49,16 @@ function mainSlider() {
         el.classList.add("active");
       } else {
         el.classList.remove("active");
+      }
+    });
+  }
+  
+  function renderCircles() {
+    circles.forEach((circle, i) => {
+      if (i === activeIndex) {
+        circle.style.backgroundColor = "black";
+      } else {
+        circle.style.backgroundColor = "white";
       }
     });
   }
